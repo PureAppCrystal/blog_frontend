4edcdde3b96a3968afc77086aff82d9ed51a9dd9
배상훈 <bsh0814@bridgetec.co.kr>
2019-02-27 18:31:35 +0900

    save

---
+++ src/components/post/PostBody/PostBody.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './PostList.scss';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+
+
+const PostBody = () => (
+    <div className={cx('post-list')}>
+     
+    </div>
+)
+
+export default PostBody;
---
+++ src/components/post/PostInfo/PostInfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './PostList.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+
+const PostInfo = () => (
+    <div className={cx('post-info')}>
+        
+    </div>
+)
+
+export default PostInfo;
