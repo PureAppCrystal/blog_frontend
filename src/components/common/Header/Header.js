dd57590ac42c37629f06ba253febba6b04e93376
배상훈 <bsh0814@bridgetec.co.kr>
2019-02-27 15:17:28 +0900

    헤더, 푸터, 버튼 생성

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -2,24 +2,23 @@ import React from 'react';
 import styles from './Header.scss';
 import classNames from 'classnames/bind'
 import { Link } from 'react-router-dom';
-
+//
+import Button from 'components/common/Button';
 
 const cx = classNames.bind(styles);
 
-const Header = () => {
-    return (
-        <header className={cx('header')}>
-            <div className={cx('header-content')}>
-                <div className={cx('brand')}>
-                    <Link to="/">reactblog</Link>
-                </div>
-                <div className={cx('right')}>
-                    {/* 조건에 따라 버튼 렌더링 */}
-                    오른쪽
-                </div>
+const Header = () => (
+    <header className={cx('header')}>
+        <div className={cx('header-content')}>
+            <div className={cx('brand')}>
+                <Link to="/">reactblog</Link>
             </div>
-        </header>
-    )
-}
+            <div className={cx('right')}>
+                {/* 조건에 따라 버튼 렌더링 */}
+                <Button theme="outline" to="/editor">새 포스트</Button>
+            </div>
+        </div>
+    </header>
+)
 
 export default Header;
