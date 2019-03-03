de6387ae100bc95ae2c1a84714a8ced8572448c3
PureAppCrystal <purecrystar@gmail.com>
2019-03-04 00:11:28 +0900

    실패

--- src/components/editor/EditorPane/EditorPane.js
+++ src/components/editor/EditorPane/EditorPane.js
@@ -3,6 +3,12 @@ import styles from './EditorPane.scss';
 import classNames from 'classnames/bind';
 //
 import CodeMirror from 'codemirror';
+
+// CodeMirror를 위한 CSS 스타일
+import 'codemirror/lib/codemirror.css';
+ import 'codemirror/theme/monokai.css';
+
+//let CodeMirror = require('codemirror');
 import 'codemirror/mode/markdown/markdown'; // 마크다운 문법 색상
 // 마크다운 내부에 들어가는 코드 색상
 import 'codemirror/mode/javascript/javascript';
@@ -10,9 +16,10 @@ import 'codemirror/mode/jsx/jsx';
 import 'codemirror/mode/css/css';
 import 'codemirror/mode/shell/shell';
 
-// CodeMirror를 위한 CSS 스타일
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/monokai.css';
+
+ 
+
+
 
 const cx = classNames.bind(styles);
 
@@ -23,11 +30,11 @@ class EditorPane extends Component {
     codeMirror = null // CodeMirror 인스턴스 
 
     initializeEditor  = () => {
-        this.codeMirror  = CodeMirror(this.editor, {
+        this.codeMirror = CodeMirror(this.editor, {
             mode: 'markdown',
             theme: 'monokai',
-            lineNumbers: true,
-            lineWrapping: true
+            lineNumbers: true, // 좌측에 라인 넘버 띄우기
+            lineWrapping: true // 내용이 너무 길면 다음 줄에 작성
         })
     }
 
@@ -39,7 +46,11 @@ class EditorPane extends Component {
         return (
             <div className={cx('editor-pane')}>
                 <input className={cx('title')} placeholder = "제목을 입력하세요" name ="title"/>
-                <div className={cx('code-editor')} ref={ref=>this.editor=ref}></div>
+                {/* <div className={cx('code-editor')} ref={ ref => this.editor = ref}></div> */}
+                <div className={cx('code-editor')} 
+                    ref={ ref => this.editor = ref}
+                >
+                </div>
                 <div className={cx('tags')}>
                     <div className={cx('description')}>태그</div>
                     <input name="tags" placeholder="태그를 입력하세요 (쉼표로 구분)" />
